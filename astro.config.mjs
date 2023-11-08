afab2e28019d5a7bd8741869e964341023f11f96
jrgavilanes@gmail.com <jrgavilanes@gmail.com>
2023-11-08 20:39:04 +0100

    integracion con vue y taildwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import vue from "@astrojs/vue";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://janrax-astro.netlify.app"
+  site: "https://janrax-astro.netlify.app",
+  integrations: [react(), vue(), tailwind()]
 });
---
+++ src/components/ButtonVue.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        {{ counter }}
+        <button class="bg-red-500 px-3 py-2 border rounded-xl text-white hover:bg-red-700" @click="sube(1)">pincha</button>
+    </div>
+</template>
+  
+<script setup>
+import { ref, watch } from 'vue'
+
+const counter = ref(0)
+
+const sube = (numero) => {
+    counter.value += numero
+}
+
+watch(counter, (nuevoValor, valorAnterior) => {
+    if (nuevoValor > 5) {
+        window.location.href = '/';
+    }
+});
+
+</script>
